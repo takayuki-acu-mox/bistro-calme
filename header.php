7018144aa1fcd1fc230b3bf721c7c8d8a56dfdbc
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-06-14 23:57:28 +0900

    サイト内検索実装

--- header.php
+++ header.php
@@ -21,10 +21,7 @@
 				<p><?php bloginfo('description'); ?></p>
 			</div>
 
-			<form class="header_search">
-				<input type="text" placeholder="キーワードを入力">
-				<i class="fas fa-search"></i>
-			</form>
+			<?php get_search_form(); ?>
 		</div>
 
 		<div class="header_links">
